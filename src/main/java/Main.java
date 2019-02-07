c9bea769f3780fb852e912519ef49a6c193257b3
joshuadiaz005 <joshua.5@hotmail.es>
2019-02-07 04:12:04 +0100

    Proyecto Terminado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,26 +9,107 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.staticFiles;
+import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Main {
 
     public static void main(String[] args) {
 
         staticFiles.location("/plantillas");
+        enableDebugScreen();
 
         final Configuration configuration = new Configuration(new Version(2, 3, 0));
         configuration.setClassForTemplateLoading(Main.class, "/");
 
-        ArrayList<Estudiante> listaDeEstudiantes = new ArrayList<>();
+        ArrayList<Estudiante> lista = new ArrayList<>();
 
         Spark.get("/", (request, response) -> {
             Template plantillaPaginaInicio = configuration.getTemplate("plantillas/index.ftl");
             StringWriter writer = new StringWriter();
 
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("nombre", "Joshua");
+            attributes.put("tamano", lista.size() > 0);
+            attributes.put("lista", lista);
             plantillaPaginaInicio.process(attributes, writer);
             return writer;
         });
+
+        Spark.get("/agregar", (request, response) -> {
+            Template plantillaPaginaInicio = configuration.getTemplate("plantillas/agregar.ftl");
+            StringWriter writer = new StringWriter();
+
+            Map<String, Object> attributes = new HashMap<>();
+            plantillaPaginaInicio.process(attributes, writer);
+            return writer;
+        });
+
+        Spark.get("/visualizar/:orden", (request, response) -> {
+            Template plantillaPaginaInicio = configuration.getTemplate("plantillas/visualizar.ftl");
+            StringWriter writer = new StringWriter();
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("estudiante", lista.get(Integer.parseInt(request.params("orden"))));
+            plantillaPaginaInicio.process(attributes, writer);
+            return writer;
+        });
+
+        Spark.post("/agregarpost", (request, response) -> {
+            String matricula = request.queryParams("matricula");
+            String nombre = request.queryParams("nombre");
+            String apellido = request.queryParams("apellido");
+            String telefono = request.queryParams("telefono");
+
+            lista.add(new Estudiante(Integer.parseInt(matricula), nombre, apellido, telefono));
+
+            response.redirect("/");
+            return "";
+        });
+
+        Spark.get("/eliminar/:orden", (request, response) -> {
+            StringWriter writer = new StringWriter();
+            lista.remove(Integer.parseInt(request.params("orden")));
+
+            response.redirect("/");
+            return writer;
+        });
+
+        Spark.get("/modificar/:orden", (request, response) -> {
+            Template plantillaPaginaInicio = configuration.getTemplate("plantillas/modificar.ftl");
+            StringWriter writer = new StringWriter();
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("estudiante", lista.get(Integer.parseInt(request.params("orden"))));
+            plantillaPaginaInicio.process(attributes, writer);
+            return writer;
+        });
+
+        Spark.post("/modificarpost", (request, response) -> {
+            StringWriter writer = new StringWriter();
+            try {
+                String matricula = request.queryParams("matricula");
+                String nombre = request.queryParams("nombre");
+                String apellido = request.queryParams("apellido");
+                String telefono = request.queryParams("telefono");
+                Estudiante estudiante = new Estudiante(Integer.parseInt(matricula), nombre, apellido, telefono);
+                for (Estudiante estudianteAModificar: lista)
+                {
+                    if(estudiante.getMatricula() == estudiante.getMatricula())
+                    {
+                        estudianteAModificar.setNombre(estudiante.getNombre());
+                        estudianteAModificar.setApellido(estudiante.getApellido());
+                        estudianteAModificar.setMatricula(estudiante.getMatricula());
+                        estudianteAModificar.setTelefono(estudiante.getTelefono());
+                        break;
+                    }
+                }
+                response.redirect("/");
+            }catch (Exception e){
+                e.printStackTrace();
+                response.redirect("/");
+            }
+            return writer;
+        });
+
+
     }
 }
--- src/main/resources/plantillas/index.ftl
+++ src/main/resources/plantillas/index.ftl
@@ -1,1 +1,93 @@
-${nombre}
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Práctica #2 - 2014-0056</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/heroic-features.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top">
+      <div class="container">
+          <a class="navbar-brand" href="/">Práctica #2 - Joshua</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Inicio
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/agregar">Agregar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Features -->
+        <div class="bg-danger text-center text-white mt-3">
+            <#if tamano>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Matricula</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Teléfono</th>
+                        <th>Modificar</th>
+                        <th>Eliminar</th>
+                        <th>Visualizar</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <#assign contadorDeEstudiantes = 0>
+
+                        <#list lista as estudiante>
+                        <tr class="animated fadeInUp">
+                            <td>${estudiante.matricula?string["0"]}</td>
+                            <td>${estudiante.nombre}</td>
+                            <td>${estudiante.apellido}</td>
+                            <td>${estudiante.telefono}</td>
+                            <td><a href="/modificar/${contadorDeEstudiantes}" class="btn btn-light" role="button">Modificar</a></td>
+                            <td><a href="/eliminar/${contadorDeEstudiantes}"  class="btn btn-light" role="button">Eliminar</a></td>
+                            <td><a href="/visualizar/${contadorDeEstudiantes}"  class="btn btn-light" role="button">Visualizar</a></td>
+                        </tr>
+                            <#assign contadorDeEstudiantes++>
+                        </#list>
+                    </tbody>
+                </table>
+            </#if>
+        </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
